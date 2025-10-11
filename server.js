d01222f209dd42ed836b7e6a274d7f31327ea599
Dipzz-newbie <mnadhiftriyudo@gmail.com>
2025-10-11 20:58:39 +0700

    Update

--- server.js
+++ server.js
@@ -1,5 +1,30 @@
-import {web} from "./src/application/web.js";
-import {logger} from "./src/application/logging.js";
+import express from "express";
+import {publicRouter} from "../route/public-api.js";
+import {errorMiddleware} from "../middleware/error-middleware.js";
+import {userRouter} from "../route/api.js";
+import cors from "cors";
+
+export const web = express();
+web.use(express.json());
+web.use(cors({
+    origin: 'http://localhost:5173', 
+    credentials: true
+}));
+
+web.use(publicRouter);
+web.use(userRouter);
+
+web.use(errorMiddleware);
+
+import winston from "winston";
+
+export const logger = winston.createLogger({
+    level: "info",
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.Console({})
+    ]
+});
 
 web.listen(3000, () => {
     logger.info("App start");
